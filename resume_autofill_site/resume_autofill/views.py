c0a67ea863c60a310e32bf038fc2627c28a2c4c4
Todd Hayton <thayton@neekanee.com>
2014-10-09 11:53:41 -0400

    resume view

--- resume_autofill_site/resume_autofill/views.py
+++ resume_autofill_site/resume_autofill/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from resume_autofill.models import Resume
 
-# Create your views here.
+def index(request):
+    return HttpResponse("Hello, world. You're at the resume_autofill index.")
+
+def view_resume(request):
+    resume = Resume.objects.all()[0]
+    context = {'resume': resume}
+    return render(request, 'view_resume.html', context)
+
+def edit_resume(request):
+    return HttpResponse("You're editing your resume.")
+    
