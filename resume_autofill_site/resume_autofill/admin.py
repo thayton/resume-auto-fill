117cf39e601bfb2dc641e0889da40e11b47e3626
Todd Hayton <thayton@neekanee.com>
2014-10-09 10:43:53 -0400

    skills and skillsets

--- resume_autofill_site/resume_autofill/admin.py
+++ resume_autofill_site/resume_autofill/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from resume_autofill.models import Resume, Education, Job, Accomplishment, SkillUsed
+from resume_autofill.models import Resume, Education, Job, Accomplishment, SkillUsed, Skillset, Skill
 
 class AccomplishmentInline(admin.StackedInline):
     model = Accomplishment
@@ -9,10 +9,17 @@ class SkillUsedInline(admin.StackedInline):
     model = SkillUsed
     extra = 1
 
+class SkillInline(admin.StackedInline):
+    model = Skill
+
+class SkillsetAdmin(admin.ModelAdmin):
+    inlines = [SkillInline]
+
 class JobAdmin(admin.ModelAdmin):
     inlines = [AccomplishmentInline, SkillUsedInline]
 
 # Register your models here.
 admin.site.register(Resume)
 admin.site.register(Education)
+admin.site.register(Skillset, SkillsetAdmin)
 admin.site.register(Job, JobAdmin)
